f2f69a080d5abbfebfeea60e5fd2863f53d4e1dc
Maksim Metelkin <maxwjiill@gmail.com>
2024-01-19 11:43:14 +0300

    updated files

--- pages/4/script.js
+++ pages/4/script.js
@@ -1,3 +1,12 @@
+function shuffleArray(array) {
+    const shuffledArray = [...array];
+    for (let i = shuffledArray.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+    }
+    return shuffledArray;
+}
+
 const phrases = [
     { latin: "\"Consuetudo est altera natura\"", translation: "\"Привычка - вторая натура\"" },
     { latin: "\"Nota bene\"", translation: "\"Заметьте хорошо!\"" },
@@ -5,59 +14,68 @@ const phrases = [
     { latin: "\"Per aspera ad astra\"", translation: "\"Через тернии к звёздам\"" }
 ];
 
-let shuffledPhrases = shuffleArray(phrases);
+let suffeldArray = shuffleArray(phrases);
 let currentIndex = 0;
-let clickCount = 0;
-let paragraphCount = 0;
+let clickC = 0;
+let t3Count = 0;
 
-function showRandomPhrase() {
-    if (currentIndex >= shuffledPhrases.length) {
+function randomPhrase() {
+    if (currentIndex >= suffeldArray.length) {
         alert("Фразы закончились");
         return;
     }
 
     const phraseContainer = document.getElementById("phrase-container");
-    const currentPhrase = shuffledPhrases[currentIndex];
+    const currentPhrase = suffeldArray[currentIndex];
     const newDiv = document.createElement("div");
 
     newDiv.textContent = currentPhrase.latin + " - " + currentPhrase.translation;
-    newDiv.className = clickCount % 2 === 0 ? "class1" : "class2";
+    newDiv.className = clickC % 2 === 0 ? "class2" : "class1";
 
     phraseContainer.appendChild(newDiv);
+
     currentIndex++;
-    clickCount++;
+    clickC++;
 }
 
-function changeFontWeight() {
+function switchFont() {
     const phraseContainer = document.getElementById("phrase-container");
-    const evenPhrases = phraseContainer.querySelectorAll(".class1");
+    const evenPhrases = phraseContainer.querySelectorAll(".class2");
 
     evenPhrases.forEach(phrase => phrase.classList.toggle("bold"));
 }
 
-function createNewParagraph() {
-    const randDiv = document.getElementById("rand");
-    const newParagraph = document.createElement("p");
-
-    if (paragraphCount >= phrases.length) {
+function createNewLine() {
+    if (t3Count >= phrases.length) {
         alert("Все фразы созданы");
         return;
     }
 
-    const currentPhrase = shuffledPhrases[paragraphCount];
+    const randDiv = document.getElementById("aside");
+    
+    const orderedListLatin = document.createElement("ol");
+    orderedListLatin.start = t3Count+1;
+    orderedListLatin.className = t3Count % 2 === 0 ? "class2" : "class1";
 
-    newParagraph.innerHTML = `<u>n=${paragraphCount}</u> <i>${currentPhrase.latin}</i> ${currentPhrase.translation}`;
-    newParagraph.className = paragraphCount % 2 === 0 ? "class1" : "class2";
-    randDiv.appendChild(newParagraph);
+    const unorderedListRussian = document.createElement("ul");
+    unorderedListRussian.className = t3Count % 2 === 0 ? "class2" : "class1";
+    
+    const currentPhrase = suffeldArray[t3Count];
 
-    paragraphCount++;
-}
+    const listItemLatin = document.createElement("li");
+    listItemLatin.innerHTML = `<i>${currentPhrase.latin}</i>`;
+    orderedListLatin.appendChild(listItemLatin);
 
-function shuffleArray(array) {  /* При открытие веб страницы массив данных каждый раз перемешивается*/ 
-    const shuffledArray = [...array];
-    for (let i = shuffledArray.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
-    }
-    return shuffledArray;
+    // const listItemRussian = document.createElement("li");
+    // listItemRussian.innerHTML = currentPhrase.translation;
+
+    const listItemRussian = document.createElement("ul");
+    listItemRussian.innerHTML = `\t<b>•</b> ${currentPhrase.translation}`;
+
+    unorderedListRussian.appendChild(listItemRussian);
+
+    randDiv.appendChild(orderedListLatin);
+    randDiv.appendChild(unorderedListRussian);
+
+    t3Count++;
 }
